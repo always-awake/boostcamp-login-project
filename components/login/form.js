07cf3a6137e54949faf9d1329c94ed9954e6e3ba
leemirim <leemirim9654@gmail.com>
2019-09-04 18:05:43 +0900

    [#37][ADD] 로그인 UI 구현

--- components/login/form.js
+++ components/login/form.js
@@ -2,20 +2,17 @@ const loginForm = () => {
     return `<div class="wrapper fadeInDown zero-raduis">
   \t  <div id="formContent">
   \t    <div class="fadeIn first">
-  \t      <!-- <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" /> -->
   \t      <h2 class="my-5">Log In</h2>
   \t    </div>
-
-  \t    <!-- Login Form -->
   \t    <form>
   \t      <input type="text" id="id" class="fadeIn second zero-raduis" placeholder="아이디">
   \t      <input type="text" id="password" class="fadeIn third zero-raduis" name="login" placeholder="비밀번호">
 \t\t      <div id="formFooter">
-  \t      \t<a class="underlineHover" href="#">Forgot Password?</a>
+            <a class="underlineHover" href="#">Forgot Password?</a>
   \t      </div>
-  \t      <input type="submit" class="fadeIn fourth zero-raduis" value="login">
+  \t      <input type="submit" class="fadeIn fourth zero-raduis" value="login" id="login_btn">
   \t      <h2>You don't have a account ?</h2>
-  \t      <input type="button" class="fadeIn fourth zero-raduis pc" value="register">
+  \t      <input type="button" class="fadeIn fourth zero-raduis pc" value="register" id="register_btn">
   \t    </form>
   \t    
 
