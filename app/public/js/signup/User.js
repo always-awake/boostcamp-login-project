e5a194168342f5176ca08e92b97637349d7cd3e1
leemirim <leemirim9654@gmail.com>
2019-09-06 00:03:47 +0900

    [MODIFY] User 객체 필드 수정 및 관련 함수 수정

--- app/public/js/signup/User.js
+++ app/public/js/signup/User.js
@@ -1,36 +1,19 @@
 const User = class {
     constructor() {
-        this.id = {
-            value: '',
-        };
-        this.password = {
-            value: '',
-        };
-        this.re_password = {
-            value: '',
-        };
-        this.name = {
-            value: '',
-        };
+        this.id = '';
+        this.password = '';
+        this.re_password = '';
+        this.name = '';
         this.birth = {
             year: '',
             month: '',
             day: '',
         };
-        this.gender = {
-            value: '',
-        };
-        this.email = {
-            value: '',
-        };
-        this.phone = {
-            value: '',
-        };
-        this.interests = {
-            value: [],
-        };
-        this.contract = {
-        };
+        this.gender = '';
+        this.email = '';
+        this.phone = '';
+        this.interests = [];
+        this.contract = false;
     };
 };
 
