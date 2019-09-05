fad37f46a5306b49d530c0f927ee0c34b451356a
leemirim <leemirim9654@gmail.com>
2019-09-06 00:08:40 +0900

    [CHOR] 불필요한 파일 삭제 및 코드 정리'

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+// 회원가입
+router.post('/', function(req, res, next) {
+  console.log(req.body);
+  res.send(req.body);
+});
+
+//아이디 중복체크 
+router.get('/validation/id', function (req, res, next) {
+
+  res.send('서버연결완료');
+});
+
+
 
 module.exports = router;
