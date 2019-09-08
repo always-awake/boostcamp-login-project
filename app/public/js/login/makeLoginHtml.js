235f24b1f1f6a3d41156dea11a12d94bd9099888
leemirim <leemirim9654@gmail.com>
2019-09-08 20:29:56 +0900

    [MODIFY] 로그인, 마이페이지 UI 수정

--- app/public/js/login/makeLoginHtml.js
+++ app/public/js/login/makeLoginHtml.js
@@ -3,13 +3,14 @@ import {setLoginEvent} from './setEvent.js';
 
 const makeLoginHTML = () => {
 
-    const signUpHead = document.getElementById('head');
-    const signUpBody = document.getElementById('body');
-    signUpHead.innerHTML = `<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+    const loginHead = document.getElementById('head');
+    const loginBody = document.getElementById('body');
+    loginHead.innerHTML = `<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
                              <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
                              <link rel="stylesheet" href="./css/login/login.css">
                              <title>네이버 : 로그인</title>`;
-    signUpBody.innerHTML = loginForm();
+    loginBody.className = 'login__body';
+    loginBody.innerHTML = loginForm();
     setLoginEvent();
 };
  export {
